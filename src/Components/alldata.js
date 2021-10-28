2cf2f314ed22e898bd00b1bd8831ca6fef7d246d
DanianB <78708804+DanianB@users.noreply.github.com>
2021-10-28 14:21:37 +1000

    Add files via upload

---
+++ src/Components/alldata.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Card from './cardcontext.js';
+
+function AllData(){
+    const [data, setData] = React.useState('');    
+
+    React.useEffect(
+        function GetData(){
+        
+        // fetch all accounts from API
+        fetch('/account/all')
+            .then(response => response.json())
+            .then(data => {
+                setData(JSON.stringify(data));                
+            });
+
+    }, []);
+
+
+    return (
+        <Card
+          bgcolor="secondary"
+          header="All data"
+          body={
+            <>{data}</>}
+        />
+    );
+}
+
+export default AllData;
